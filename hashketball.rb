ab6e161b3c273656f43a8bda9811b26cba00cede
Anna Nicole Reed <reedanna@mail.com>
2020-07-02 02:04:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -77,7 +77,7 @@ def game_hash
           :slam_dunks => 2
         },
         {
-          :player_name => "Bismack Miyombo",
+          :player_name => "Bismack Biyombo",
           :number => 0,
           :shoe => 16,
           :points => 12,
