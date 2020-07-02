539174917dc80876f52ce2fc8bc63d9c17e5785a
Anna Nicole Reed <reedanna@mail.com>
2020-07-02 01:59:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -32,10 +32,10 @@ def game_hash
           :shoe => 17,
           :points => 17,
           :rebounds => 19,
-          :assists => 12,
-          :steals => 12,
-          :blocks => 12,
-          :slam_dunks => 7
+          :assists => 10,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 15
         },
         ]
     },
